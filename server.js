901afa0a422e5df1728a2196dd2b5f13f18384cb
TakahashiIkki <new_ittyandesu@outlook.jp>
2018-06-05 00:59:43 +0900

    file-systemを用いてhtmlファイルを読み込むように修正。

--- server.js
+++ server.js
@@ -1,26 +1,49 @@
 const http = require('http');
+const filSystem = require('fs');
+
 const server = http.createServer();
 let message = '';
 
 server.on('request', function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
     switch (request.url) {
         case '/about':
             message = 'Welcome About Page.';
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.write(message);
+            response.end();
             break;
         case '/company':
             message = 'Welcome Company Page.';
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.write(message);
+            response.end();
             break;
         case '/recruit':
             message = 'Welcome Recruit Page';
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.write(message);
+            response.end();
+            break;
+        case '/service':
+            filSystem.readFile( __dirname + '/service.html', 'utf-8', function(error, data) {
+                // エラー発生時
+                if (error) {
+                    response.writeHead(404, {'Content-Type' : 'text/plain'});
+                    response.write('page not found22');
+                    return response.end();
+                }
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.write(data);
+                response.end();
+            });
             break;
         default:
             message = 'Page Not Found';
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.write(message);
+            response.end();
             break;
     }
-    response.write(message);
-    response.end();
 });
 
 server.listen(3000);
